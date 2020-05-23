4b9968aadf190d4bd6983f8e7d75b6cad980a961
maalopez <maalopez@emcali.com.co>
2020-05-23 09:51:17 -0500

    last

---
+++ simple/src/test/java/co/edu/usbcali/scm/AppTest.java
@@ -0,0 +1,28 @@
+package co.edu.usbcali.scm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue02()
+    {
+        assertTrue( true );
+    }
+}
